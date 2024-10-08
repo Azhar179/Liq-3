f168c139cf8bd7521f40383b87446356d76a2168
bhagwana <azhar.bhagwan@dowjones.com>
2024-10-08 15:52:36 +0530

    changes

--- Liquibase_Demo/src/main/resources/db/changelog/sample_proc.sql
+++ Liquibase_Demo/src/main/resources/db/changelog/sample_proc.sql
@@ -1,7 +1,4 @@
-use twenty_eight;
-DELIMITER &&
-CREATE PROCEDURE test_proc_1 (IN tid INT)
+CREATE PROCEDURE test_proc_1(IN tid INT)
 BEGIN
 select * from test_table where test_id = tid;
-END &&
-DELIMITER ;
+END$$
